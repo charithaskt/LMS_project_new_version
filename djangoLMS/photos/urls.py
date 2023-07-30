c8e28acfcd42dd8ea0790a9a8c1c027560fa573d
charithaskt <44762610+charithaskt@users.noreply.github.com>
2023-07-31 01:12:43 +0530

    Add files via upload

---
+++ djangoLMS/photos/urls.py
@@ -0,0 +1,13 @@
+#from django.conf.urls import url
+from django.urls import re_path 
+
+from . import views
+
+urlpatterns = [
+    re_path(r'^clear/$', views.clear_database, name='clear_database'),
+    re_path(r'^delete-patron-photo/(?P<pk>\d+)/$', views.delete_patron_photo, name='delete_patron_photo'),
+
+    re_path(r'^basic-upload/$', views.BasicUploadView.as_view(), name='basic_upload'),
+    re_path(r'^progress-bar-upload/$', views.ProgressBarUploadView.as_view(), name='progress_bar_upload'),
+    re_path(r'^drag-and-drop-upload/$', views.DragAndDropUploadView.as_view(), name='drag_and_drop_upload'),
+]
